739e7f0ab611d49825ade478e25730f8910b7549
Your Name <you@example.com>
2023-12-07 14:15:14 -0800

    moalaA

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if str[i] >= "a" and str[i] <= "z":
-            print("{}".format(chr(ord("A") + (ord(str[i]) - ord("a")))), end="")
-        else:
-            print("{}".format(str[i]), end="")
+        print("{}".format(chr(ord("A") + (ord(str[i]) - ord("a")))) if  str[i] >= "a" and str[i] <= "z" else "{}".format(str[i]), end="")
     print()
