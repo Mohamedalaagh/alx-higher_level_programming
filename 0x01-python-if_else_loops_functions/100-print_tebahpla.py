9987834c162cb565835e7156ef1ec857de9ef84d
Your Name <you@example.com>
2023-12-08 16:21:37 -0800

    moalaa

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-x = 0
-z = 0
-for i in range(ord("a"), ord("z") + 1):
-    print(chr(ord("a") + 25 - x) if z % 2 == 0 else chr(ord("A") + (ord("a") + 25 - x) - ord("a")), end="")
-    x += 1
-    z += 1
+for i in range(122, 96, -1):
+    if i % 2:
+        i = i - 32
+    print("{:c}".format(i), end="")
