9559f09fe2a328f4767a18285c3471a588467a8f
Your Name <you@example.com>
2024-02-05 11:26:09 -0800

    moalaa

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def remove_char_at(str, n):
+    n_str = ""
+    for i in range(len(str)):
+        if i == n:
+            continue
+        else:
+            n_str += str[i]
+
+    return n_str
