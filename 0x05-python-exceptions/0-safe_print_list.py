097532c2262c42b9ab97feb110cd5b5937b55fa2
Moalaa <moalaa4621@gmail.com>
2024-03-04 13:40:37 -0800

    mpalaa

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     j = None
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            print(f"{my_list[i]}", end="")
             j = i
         except IndexError:
             break
