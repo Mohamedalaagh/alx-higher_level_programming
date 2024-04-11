9b7c91cccb52d2cf161d88b4342628dd26be40f2
moalaa <moaladed4621@gmail.com>
2024-04-11 13:46:40 -0700

    new class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+
+    " Calculate the square "
+
+    def __init__(self, size=0):
+        """ Creating a private attribute
+        args:
+            size(int): the size of the new square.
+        """
+        self.__size = size
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
