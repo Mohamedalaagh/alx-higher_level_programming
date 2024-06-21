e8aabdea5fc8beb0aa8b964b2c59fe9046d0b3be
moalaa <moaladed4621@gmail.com>
2024-06-21 12:46:09 -0700

    moala

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,11 @@ class Square:
     args:
         size(int): the size of the new square.
     """
+
     def __init__(self, size=0):
         if not type(size) == int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
