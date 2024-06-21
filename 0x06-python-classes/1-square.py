b515a27a3dd4f2b97ed1430b5b90f9f0763e45f2
moalaa <moaladed4621@gmail.com>
2024-06-21 12:18:42 -0700

    moalaa

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Creating a class square """
+
+
 class Square:
     " Calculate the square "
     def __init__(self, size=0):
@@ -8,4 +10,3 @@ class Square:
             size(int): the size of the new square.
         """
         self.__size = size
-
