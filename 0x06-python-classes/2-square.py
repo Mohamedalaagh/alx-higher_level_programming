5fe3ae631728efd87b8ba829313819651bbd6b0f
moalaa <moaladed4621@gmail.com>
2024-06-21 12:40:22 -0700

    moalaa

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-# 2-square.py
-"""Square class definition"""
+"""Define a square"""
 
 
 class Square:
-    " Calculate the square "
-
+    """Creating a private attribute with specific condition
+    args:
+        size(int): the size of the new square.
+    """
     def __init__(self, size=0):
-        """ Creating a private attribute
-        args:
-            size(int): the size of the new square.
-        """
-        if not isinstance(size, int):
+        if not type(size) == int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
