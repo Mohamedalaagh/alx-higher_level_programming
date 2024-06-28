5faecb65e0038c157d3528ce4fac95b66dc9bdd9
Your Name <you@example.com>
2024-06-28 13:04:54 -0700

    moala

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
+"""
+This script takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
 
+import MySQLdb as db
+from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+if __name__ == '__main__':
+    # Connect to the MySQL database
+    db_connection = db.connect(host="localhost", port=3306,
+                               user=argv[1], passwd=argv[2], db=argv[3])
+    db_cursor = db_connection.cursor()
+
+    # Formulate the SQL query
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC".format(argv[4])
+    db_cursor.execute(query)
+    states = db_cursor.fetchall()
+
+    # Print each state found
+    for state in states:
+        print(state)
