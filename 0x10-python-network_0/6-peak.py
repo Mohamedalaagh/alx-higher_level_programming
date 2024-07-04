d54cc54af1765a843f40b150aec663e266b13ebe
moalaa <moaladed4621@gmail.com>
2024-07-04 11:44:36 -0700

    moalaa

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    Method to find hightest number in array
+    """
+    if bool(list_of_integers) is False:
+        return None
+    list_of_integers.sort()
+    return list_of_integers[-1]
