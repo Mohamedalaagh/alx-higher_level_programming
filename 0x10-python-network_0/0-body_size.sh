007bcfa573cfa1fdf77776325f99a1dbbbbf47e5
moalaa <moaladed4621@gmail.com>
2024-07-04 11:19:00 -0700

    moalaa

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Check if a URL was provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send a request to the URL and get the size of the body of the response in bytes
-curl -s -o /dev/null -w "%{size_download}\n" "$1"
-
+# Get the comtent-lenght of a giving ip address
+curl -sI "$1" | awk '/Content-Length/{print $2}'
