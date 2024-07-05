151284f78094b0a3c1686263ed5a67fde12f89c0
moalaa <moaladed4621@gmail.com>
2024-07-05 07:46:21 -0700

    moalaa

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,41 +1,13 @@
 #!/usr/bin/python3
 """
-Script that fetches the status of a specified URL.
-
-This script sends an HTTP GET request to the URL \
-        https://intranet.hbtn.io/status
-and prints the body of the response. The response\
-        details include the type of the
-content, the raw content, and the UTF-8 decoded content.
-
-Modules required:
-    urllib.request: This module provides functions and classes to manage HTTP
-    requests.
-
-Functions:
-    main(): Sends a GET request to the URL and prints\
-            the details of the response.
-
-Usage:
-    Run the script directly from the command line to see the output of the GET
-    request.
-
-Example:
-    $ ./script_name.py
+Script that fetches https://intranet.hbtn.io/status
 """
 import urllib.request
 
 
 def main():
     """
-    Function to print the response of a specific URL.
-
-    This function sends an HTTP GET request \
-            to the URL https://intranet.hbtn.io/
-    status. It then reads the response and prints:
-    - The type of the response content.
-    - The raw content of the response.
-    - The UTF-8 decoded content of the response.
+    Funtion to print a response of a specific url
     """
     url = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
@@ -44,7 +16,6 @@ def main():
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
         print('\t- utf8 content: {}'.format(html.decode('utf8')))
-
 
 if __name__ == "__main__":
     main()
