25fca155b1d34b85149910b5c57832047630e491
Your Name <you@example.com>
2024-08-15 20:30:59 +0300

    mpalaa

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,11 +16,11 @@ class Rectangle(Base):
 
         Return: always nothing.
         """
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
         @property
         def width(self):
