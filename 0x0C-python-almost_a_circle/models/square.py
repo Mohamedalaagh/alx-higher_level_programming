ade81029b63be1a23b965df9b372f6873d172e50
Your Name <you@example.com>
2024-08-16 18:12:10 +0300

    moala

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialize new reectangle"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        if not isinstance(value, int):
+            raise TypeError("Size must be integer")
+        if value < 0:
+            raise ValueError("Size must be positive value")
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Return a string represintation for user"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
