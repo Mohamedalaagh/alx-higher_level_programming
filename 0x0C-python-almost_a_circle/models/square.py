ae2ee412bc6e0f7d2786a3a6417c928aec90f44d
Your Name <you@example.com>
2024-08-19 14:27:21 +0300

    mnoala

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,6 +33,15 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+    def to_dictionary(self):
+        """return a dictionary representation of square"""
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
+
     def __str__(self):
         """Return a string represintation for user"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
